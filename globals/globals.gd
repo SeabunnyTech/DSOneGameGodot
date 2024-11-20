99275db8064cf2a68b851d68c3578ea07981ae48
Kevin Cheng <kevinphy@gmail.com>
2024-11-20 19:38:30 +0800

    1. 分離 login.gd 中絕大多數的動畫到 login_animation.gd 2. 修改 project setting 中 stretch mode 為 viewport（才不會受到視窗拖曳影響） 3. Electron 電仔的部分目前進度僅 40%

--- globals/globals.gd
+++ globals/globals.gd
@@ -1,8 +1,18 @@
 extends Node
 
-
-var viewport_size: Vector2 = Vector2(3840, 2160)
-
 # To control players with mouse
-@export var mouse_mode: bool = false
-@export var debug_message: bool = false
+@export var mouse_mode: bool = true
+@export var debug_message: bool = true
+
+var _viewport_size: Vector2 = Vector2(3840, 2160)
+
+# 目前用不到，因為 stretch 模式設定為 viewport，所以會鎖定在 3840x2160
+# 但難保之後會變成 canvas_item，所以先保留
+signal viewport_size_changed(new_size: Vector2)
+
+func get_viewport_size() -> Vector2:
+    return _viewport_size
+
+func set_viewport_size(new_size: Vector2) -> void:
+    _viewport_size = new_size
+    viewport_size_changed.emit(new_size)
