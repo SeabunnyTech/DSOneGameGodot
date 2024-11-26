9c4963d1c71ae5d002f0adccf45fd11680076eae
fatcloud <victor19871226@hotmail.com>
2024-11-27 05:35:21 +0800

    用 _process 做狀態判斷而非 event driven + 判斷情境做狀態轉換

--- scenes/levels/login_reworked.gd
+++ scenes/levels/login_reworked.gd
@@ -5,58 +5,110 @@ extends Node2D
 #
 # 1. 玩家的游標外觀
 # 2. 場內的物件被觸發以後要執行什麼動作
-#
+
+enum State {
+	WAIT_ONE_PLAYER,
+	WAIT_TWO_PLAYER,
+	GO_NEXT_SCENE
+}
 
 
-var player_num = 1
+var LState = DSOneLogo.State
+@onready var logo1 = $DSOneLogo1
+@onready var logo2 = $DSOneLogo2
 
-@onready var logo = $DSOneLogo
+var PState = Player.State
+var player1 = PlayerManager.player1
+var player2 = PlayerManager.player2
 
-# Called when the node enters the scene tree for the first time.
+
+@onready var window_size = DisplayServer.window_get_size()
+@onready var logo2_offset = Vector2i(0.2 * window_size[0], 0)
+
 func _ready() -> void:
-	# 連接信號
-	# 1. 玩家的進入與離開
-	# 2. Logo 被觸發與被遠離
+	# 設定 logo 的色相與 player 相符
+	logo1.hue = PlayerManager.player_hue[0]
+	logo2.hue = PlayerManager.player_hue[1]
 
-	# 讓第一個 Logo 進場
-	logo.heads_to_state(logo.State.IDLE)
+	# 設定 logo 位置
+	logo1.position = window_size/2
+	logo2.position = window_size/2 + logo2_offset
+
+
+var heading_state
+func _heads_to_state(new_state: State):
+	if heading_state == heading_state:
+		return
+	heading_state = new_state
 
 
 
-func _on_first_player_state(enter: bool):
+func _process(_delta: float) -> void:
+	# 觀察場上的玩家與 logo 狀態, 決定下一步要往哪走
 
-	if enter:
-		# 第一 Logo => INVITING
-		# INVITING 動畫結束才 Triggerable
-		pass
+	# 先處理個別 logo 的反應
+	## logo1 在沒玩家時往 fade, 有玩家時看 trigger 狀態前往 invite 或 triggered 
+	var logo1_heading_state
+	var logo1_triggered = false
+	if player1.state in [PState.FADED, PState.LOST]:
+		logo1_heading_state = LState.IDLE
 	else:
-		# 第一 Logo => FADED
-		# FADED 動畫隨時可以被中斷
-		pass
+		logo1_triggered = logo1.overlaps_trigger_area(player1)
+		logo1_heading_state = LState.TRIGGERED if logo1_triggered else LState.INVITING
+	logo1.heads_to_state(logo1_heading_state)
 
-
-
-
-func _on_second_player_state(enter: bool):
-	if enter:
-		# 第一 Logo 移左, 第二 Logo => INVITING
-		pass
+	## logo2 在玩家二不在時藏起, 在場時則同樣會選擇要去 invite 或 triggered
+	var logo2_heading_state
+	var logo2_triggered = false
+	if player2.state in [PState.FADED, PState.LOST]:
+		logo2_heading_state = LState.HIDDEN
 	else:
-		# 第一 Logo 回中, 第二 Logo => HIDDEN 
-		pass
+		logo2_triggered = logo2.overlaps_trigger_area(player2)
+		logo2_heading_state = LState.TRIGGERED if logo2_triggered else LState.INVITING
+	logo2.heads_to_state(logo2_heading_state)
+
+	# 接著要處理兩個 logo 的位置, 一個玩家以下和兩個玩家在場會讓 logo 調整位置
+	if player2.state == PState.LOST:
+		_heads_to_one_logo()
+	else:
+		_heads_to_two_logo()
+
+	# 再來是更新 player 的觸發反應
+	PlayerManager.update_player_trigger_state(player1, logo1_triggered)
+	PlayerManager.update_player_trigger_state(player2, logo2_triggered)
+
+	# 最後判定是否要進入下一關
+	if logo1.state == LState.TRIGGERED:
+		if player2.state == PState.LOST:
+			# Go next scene with 1P
+			pass
+		elif logo2 == LState.TRIGGERED:
+			# Go next scene with 2p
+			pass
 
 
+# 調整 logo 位置的函數
 
-func _on_first_logo_trigger_state(triggered: bool):
-	# 玩家碰到 Logo 時 Logo 會自己進入 Triggering 動畫
-	# Triggering 動畫完成時抵達此處
+var heading_logo_layout = 1		# 1 代表一個 logo, 2 代表兩個 logo
+var logo_position_tween
 
-	# 此時有兩種情況
-	# 1. 第二玩家在場 => 等待第二玩家就位
-	# 2. 第二玩家不在場 => 轉場去下一個畫面
-	pass
+func _move_logo1(pos):
+	if logo_position_tween:
+		logo_position_tween.kill()
+	logo_position_tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+	logo_position_tween.tween_property(logo1, 'position', Vector2(pos[0], pos[1]), 1)
+	#logo_position_tween.parallel()
 
 
+func _heads_to_one_logo():
+	if heading_logo_layout == 1:
+		return
+	heading_logo_layout = 1
+	_move_logo1(window_size/2)
 
-func _on_second_logo_trigger_state(triggered: bool):
-	pass
+
+func _heads_to_two_logo():
+	if heading_logo_layout == 2:
+		return
+	heading_logo_layout = 2
+	_move_logo1(window_size/2-logo2_offset)
