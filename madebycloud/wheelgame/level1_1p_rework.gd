aa293cd42c75f8f193eb83dab0aaf03c49663ad1
fatcloud <victor19871226@hotmail.com>
2024-12-06 11:43:33 +0800

    把音樂函數往外分離 / 移除已用不到的小物件 WhiteCurtain

--- madebycloud/wheelgame/level1_1p_rework.gd
+++ madebycloud/wheelgame/level1_1p_rework.gd
@@ -7,10 +7,15 @@ extends Node2D
 # 3. 教學結束就來到倒數及開始 ui
 # 4. 遊戲開始後, 音樂可以切換成遊戲音樂
 
+# 宣告成 Export 再用面板設定音源可以避免檔案路徑一被整理這邊就要重寫路徑的問題
+@export var game_music: AudioStream
+@export var tutorial_music: AudioStream
+
+
 @export var camera_center = Vector2(1920, 1080):
 	set(new_center):
 		camera_center = new_center
-		
+
 @onready var guide_message = $Title
 @onready var wheelgame_env = $WheelGameEnviromnent
 @onready var circular_mask = $CircularMask
@@ -19,11 +24,12 @@ extends Node2D
 # reset 非常重要, 定義了動畫能夠順利執行的起始條件
 # 在編輯器預覽的設定和 reset 後的設定可以不一樣
 func reset():
-	$WhiteCurtain.modulate.a = 0
+	modulate.a = 0
 	guide_message.modulate.a = 0
 	$HUD.reset()
+	circular_mask.alpha = 1.0
 	circular_mask.tween_center_radius(Vector2(1920, 1080), 0.0, 0.0)
-	wheelgame_env.rotation_enabled = false
+	wheelgame_env.reset()
 
 
 func _undate_guide_text(new_text_state):
@@ -72,39 +78,11 @@ func _undate_guide_text(new_text_state):
 
 
 
-func _fade_audio_stream(audio_player, fade_in=false, duration = 2):
-	var audio_tween = create_tween()
-	var start_volume_db = -80.0 if fade_in else -10.0
-	var target_volume_db = -10.0 if fade_in else -80.0
 
-	audio_player.volume_db = start_volume_db
-	audio_tween.tween_property(audio_player, "volume_db", target_volume_db, duration)
-	if fade_in:
-		audio_player.play()
-	else:
-		audio_tween.tween_callback(audio_player.stop)
-
-
-
-
-func on_enter():
+func enter_scene():
 	var tween = create_tween()
-	_fade_audio_stream($TutorialMusic, true)
-	#curtain
-	# 2. 裡面有一台巨大的渦輪機, 當水流推著它轉動時, 就會產生電力喔
-	# 3. 現在就用你手上的水滴畫一個大圓來推動它吧!
-	# 4. (噴出電仔, 立即運鏡到空中) 哇! 你成功發出了十顆電仔呢! 看來你已經了解這個遊戲怎麼玩了喔!
-	# 5. 接下來給你 30 秒的時間, 看看你能發出多少電吧!
-	# 6. 3, 2, 1, go! (配樂變化)
-
-
-
-	# 轉場進入
-
-	
-	#tween.tween_method(circular_mask, 'center', )
-	#circular_mask.center = Vector2(2000, 1000)
-	#tween.tween_method(func(center): circular_mask.center = center, Vector2(1920, 1080), Vector2(0 ,0), 1)
+	GlobalAudioPlayer.play_music(tutorial_music)
+	tween.tween_property(self, 'modulate:a', 1, 1)
 
 	# 1. (白底) 歡迎來到蓄電大挑戰! 在這關我們將化身一顆水滴來驅動渦輪發電喔! (3 sec)
 	tween.tween_interval(1)
@@ -194,10 +172,9 @@ func continue_tutorial():
 		wheelgame_env.camera_to(Vector2(2200, 1080), 1.2)
 		wheelgame_env.rotation_enabled = false
 		circular_mask.tween_center_radius(Vector2(2420, 1080), 0.0, 1)
-		_fade_audio_stream($TutorialMusic)
+		GlobalAudioPlayer.fade_out()
 	)
 	tween.tween_interval(1)
-	tween.tween_callback(func(): $WhiteCurtain.modulate.a = 1)
 
 	# 11. 接下來就進入挑戰吧! 看看你能在一分鐘內發出多少電力呢!
 	tween.tween_callback(func():_undate_guide_text('final'))
@@ -212,13 +189,13 @@ func continue_tutorial():
 	tween.tween_property(guide_message, 'modulate:a', 0, 1)
 
 	# 瞬間關閉圓形遮罩, 慢慢關閉白幕
-	tween.tween_property(circular_mask, 'modulate:a', 0, 0)
-	tween.tween_property($WhiteCurtain, 'modulate:a', 0, 1)
+	tween.tween_property(circular_mask, 'alpha', 0, 1)
+	#tween.tween_property($WhiteCurtain, 'modulate:a', 0, 1)
 
 	tween.tween_callback(func():
 		wheelgame_env.rotation_enabled = true
 		$HUD.show()
-		_fade_audio_stream($GameMusic, true, 1)
+		GlobalAudioPlayer.play_music(game_music)
 	)
 	
 
@@ -238,20 +215,17 @@ func _ready() -> void:
 	# 2. 計分板
 
 	# 電仔生成
-	player.full_rotation_completed.connect(func(player, clockwise):
-		wheelgame_env.generate_electron_and_adjust_lake_level(clockwise)
+	player.full_rotation_completed.connect(func(_player, clockwise):
+		wheelgame_env.react_to_wheel_rotation(clockwise)
 	)
 
 	# 水輪旋轉
-	player.rotation_detected.connect(func(player, clockwise, speed):
+	player.rotation_detected.connect(func(_player, clockwise, speed):
 		wheelgame_env.rotate_wheel(clockwise, speed)
 	)
 
-	
-	on_enter()
-
-
+	enter_scene()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
