4316ac4b6a0c6cfb257ae0bada3b96f1f23fdd77
fatcloud <victor19871226@hotmail.com>
2024-12-12 10:57:16 +0800

    移除白色窗簾遮蓋改用主題淡入的做法節省資源

--- scenes/levels/Login/welcome.gd
+++ scenes/levels/Login/welcome.gd
@@ -5,25 +5,18 @@ signal go_next_scene
 
 @export var disabled = true
 
-@onready var guide_texture_rect = $CenterContainer/VBoxContainer/TextureRect
-@onready var white_curtain = $WhiteRectCurtain
-
-var guide_images = [
-	load("res://assets/images/drawings/解說圖1-1.PNG"),
-	load("res://assets/images/drawings/解說圖1-2.PNG"),
-	load("res://assets/images/drawings/解說圖1-3.PNG")
-]
+@onready var guide_ui = $CenterContainer/VBoxContainer
 
 
 func enter_scene():
 	# 進場的前提是畫面已經是一片白底
 	# 這個畫面將從一片 white_curtain 進入
-	modulate.a = 1
+	visible = true
 
 	# 淡出白色前景
 	var tween = create_tween()
 	tween.tween_interval(0.3)
-	tween.tween_property(white_curtain, 'modulate:a', 0, 1)
+	tween.tween_property(guide_ui, 'modulate:a', 1, 1)
 	tween.finished.connect(func():
 		disabled = false
 	)
@@ -32,25 +25,28 @@ func enter_scene():
 
 var time_since_process = 0
 var duration_each_img = 0.3
-@export var loop_img =false
+
+
+func _ready():
+	reset()
+	enter_scene()
+
 
 func _process(delta: float) -> void:
 	if disabled:
 		return
 
+	if Engine.is_editor_hint():
+		return
+
 	if PlayerManager.num_active_players() > 0:
 		leave_for_next_scene()
-
-	if loop_img:
-		time_since_process += delta
-		var image_index = int(floor(time_since_process / duration_each_img)) % 3
-		guide_texture_rect.texture = guide_images[image_index]
 
 
 
 func leave_for_next_scene():
 	var tween = create_tween()
-	tween.tween_property(white_curtain, 'modulate:a', 1, 0.5)
+	tween.tween_property(guide_ui, 'modulate:a', 0, 0.5)
 	tween.tween_interval(0.5)
 	tween.finished.connect(
 		func():
@@ -61,5 +57,6 @@ func leave_for_next_scene():
 
 
 func reset():
-	modulate.a = 0
+	visible = false
+	guide_ui.modulate.a = 0
 	disabled = true
