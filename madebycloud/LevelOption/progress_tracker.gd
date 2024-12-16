5d7b069963caf4b2a82da57b8c09b3f421301910
fatcloud <victor19871226@hotmail.com>
2024-12-16 19:10:20 +0800

    選關按鈕配件: 自帶 tween 的 progress tracker

---
+++ madebycloud/LevelOption/progress_tracker.gd
@@ -0,0 +1,28 @@
+extends Node
+
+signal triggered
+signal heading_altered(target_state)
+
+@export var duration: float = 0.5
+
+var progress: float = 0.0
+var target_progress: float = 0.0
+var tween
+
+func react(triggering: bool):
+	var new_target_progress = 1 if triggering else 0
+	if new_target_progress != target_progress:
+		target_progress = new_target_progress
+		if tween:
+			tween.kill()
+		tween = create_tween()
+		tween.tween_property(self, 'progress', target_progress, duration * (1-progress))
+
+		heading_altered.emit(triggering)
+
+		# 走在觸發的路上
+		if triggering:
+			tween.tween_callback(triggered.emit)
+
+func _process(delta: float) -> void:
+	pass
