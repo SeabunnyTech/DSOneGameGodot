378e608fd01402b81fc186754a26824bab721bbf
fatcloud <victor19871226@hotmail.com>
2024-12-19 15:24:12 +0800

    增加一個 global 變數紀錄 logos 畫面登入選定的玩家數

--- globals/globals.gd
+++ globals/globals.gd
@@ -4,6 +4,9 @@ extends Node
 @export var mouse_mode: bool = false
 @export var debug_message: bool = false
 
+# 特設的變數, 用來記錄 logos 這個畫面登入的玩家數, 方便其它頁面參考這個數字做出反應
+var intended_player_num: int = 1
+
 var _viewport_size: Vector2 = Vector2(3840, 2160)
 
 # 目前用不到，因為 stretch 模式設定為 viewport，所以會鎖定在 3840x2160
