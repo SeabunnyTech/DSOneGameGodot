54caa5c8111daba205ba7e94c4d7ca9986e20f3a
fatcloud <victor19871226@hotmail.com>
2025-12-13 05:46:38 +0800

    排好了 Layout 但還沒改內部

---
+++ levels/solargame/level3_2p.gd
@@ -0,0 +1,186 @@
+extends Control
+
+
+signal leave_for_level(new_level_name)
+
+
+
+# 這個 level 的一些行為:
+# 1. 剛進入時會有一些教學, 基本上就是 文字 ui 和運鏡的 tween 一個接一個
+# 2. 可以考慮給一個跳過按鈕
+# 3. 教學結束就來到倒數及開始 ui
+# 4. 遊戲開始後, 音樂可以切換成遊戲音樂
+
+# 宣告成 Export 再用面板設定音源可以避免檔案路徑一被整理這邊就要重寫路徑的問題
+@export var game_music: AudioStream
+
+@onready var guide_message = $Title
+@onready var solarfarm_env = $SolarfarmEnv
+@onready var circular_mask = $CircularMask
+@onready var player_waiter = $"1p_player_waiter"
+
+@onready var time_board = $TimeBoard
+
+# reset 非常重要, 定義了動畫能夠順利執行的起始條件
+# 在編輯器預覽的設定和 reset 後的設定可以不一樣
+func reset():
+
+	# 隱藏關卡本身
+	visible = false
+	solarfarm_env.set_collision_enabled(false)
+
+	# 介紹用的訊息與遮罩
+	guide_message.modulate.a = 0
+	circular_mask.alpha = 1.0
+	circular_mask.tween_center_radius(Vector2(1920, 1080), 0.0, 0.0)
+
+	# 遊戲本體及記分板
+	solarfarm_env.reset()
+	time_board.reset()
+	time_board.modulate.a = 1
+
+	# 備用的邏輯
+	player_waiter.reset()
+
+
+
+var tween
+func enter_scene():
+
+	# 把玩家的游標設成水滴
+	for p in PlayerManager.current_players:
+		p.set_player_appearance(true, {'color':Color.WHITE})
+
+	visible = true
+	solarfarm_env.set_collision_enabled(true)
+
+	if tween:
+		tween.kill()
+	tween = create_tween()
+	tween.tween_property(circular_mask, 'alpha', 0, 1)
+	tween.tween_callback(_game_start)
+
+
+
+func leave_scene_for_restart():
+	circular_mask.tween_radius(0.0, 1.0, func():
+		reset()
+		leave_for_level.emit('welcome')
+	)
+
+
+func _game_start():
+	solarfarm_env.start_game_play()
+
+	time_board.start()
+	$short_whistle.play()
+	GlobalAudioPlayer.play_music(game_music)
+	
+
+
+@onready var player = PlayerManager.player1
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+
+	if not Engine.is_editor_hint():
+		reset()
+
+		# 連接UI 及遊戲環境
+		time_board.timeout.connect(_game_timeout)
+		#wheelgame_env.electron_collected.connect($HUD.add_score)
+
+		# 連接 player lost 計時器
+		player_waiter.player_lost_for_too_long.connect(leave_scene_for_restart)
+
+
+	# 測試的時候才會成為 main scene
+	if get_tree().current_scene == self:
+		reset()
+		enter_scene()
+
+
+var game_stop_tween
+func _game_timeout():
+	solarfarm_env.stop_game_play()
+	$long_whistle.play()
+
+	# 延遲後退相機畫面
+	if game_stop_tween:
+		game_stop_tween.kill()
+	game_stop_tween = create_tween()
+
+	game_stop_tween.tween_callback(func():
+		solarfarm_env.camera_to(Vector2(1920,1080), 1.0, 2)
+	)
+
+	game_stop_tween.tween_property(time_board, 'modulate:a', 0, 0.5)	
+	game_stop_tween.tween_interval(0.5)
+
+	solarfarm_env.show_score_board()
+	game_stop_tween.tween_callback(solarfarm_env.collect_electrons)
+
+	# 挑戰完成! 10 秒後
+
+	GlobalAudioPlayer.stop()
+	await solarfarm_env.all_electron_collected
+	_congrats_and_return()
+
+
+
+func _congrats_and_return():
+
+	var end_duration = 2
+	var thanks_duration = 4
+	var wait_duration = 2
+
+	if tween:
+		tween.kill()
+	tween = create_tween()
+	tween.tween_interval(wait_duration)
+	tween.tween_callback(func():
+		$"victory sfx".play()
+		for p in PlayerManager.current_players:
+			p.set_player_appearance(true, {'color':Color.BLACK})
+	)
+	_show_text('congrats', end_duration, 1)
+	_show_text('thanks')
+
+	if game_stop_tween:
+		game_stop_tween.kill()
+	game_stop_tween = create_tween()
+	game_stop_tween.tween_interval(wait_duration)
+	game_stop_tween.tween_property(circular_mask, 'alpha', 1, 1)
+	game_stop_tween.tween_interval(end_duration)
+	game_stop_tween.tween_interval(thanks_duration)
+	game_stop_tween.tween_callback(leave_scene_for_restart)
+
+
+
+func _update_guide_text(new_text_state):
+	var titles = {
+		'congrats' : '挑戰完成!',
+		'thanks' : '感謝你的參與',
+	}
+
+	var guides = {
+		'congrats' : '你讓太陽能板接到了' + str(solarfarm_env.score) + '個光子呢!',
+		'thanks':'電幻 1 號所祝您有美好的一天!',
+	}
+
+	var guide_text_positions = {
+		'congrats' : Vector2(960, 920),
+		'thanks':Vector2(960, 920),
+	}
+
+	$Title.position = guide_text_positions[new_text_state]
+	$Title.text = titles[new_text_state]
+	$Title/Label.text = guides[new_text_state]
+
+
+
+func _show_text(text_key, duration=2, trans_duration=1):
+	tween.tween_callback(func():_update_guide_text(text_key))
+	tween.tween_property(guide_message, 'modulate:a', 1, trans_duration)
+	tween.tween_interval(duration)
+	tween.tween_property(guide_message, 'modulate:a', 0, trans_duration)
+	return tween
---
+++ levels/solargame/solarfarm_env_2p.gd
@@ -0,0 +1,183 @@
+@tool
+extends Control
+class_name SolarfarmEnv_2p
+
+# 設計給上層遊戲場景 (solargame.tscn) 呼叫的 API
+
+## 當陽光打到太陽能板上時發出
+signal on_light_hit_solar_panel
+signal all_electron_collected
+
+@onready var cloud_manager : CloudManager = $CloudManager
+@onready var clouds_cleared = cloud_manager.clouds_cleared
+
+# 天空背景，可能是 ColorRect 或一個著色器
+@onready var sky_background = $sky_background
+
+# 雲的容器，所有動態產生的雲實例都會放在這個節點下
+@onready var clouds_container: Node2D = $clouds_container
+
+# 山上樹木的容器，方便統一處理風吹動畫
+@onready var trees_container: Node2D = $trees_container
+
+
+var electron_collected : int = 0
+
+func _ready():
+	$SolarPanel2d.hit_by_sunlight.connect(func(): electron_collected+=1)
+
+
+func set_responsive_to_anticyclone(responsive:bool):
+	$CloudManager.set_responsive_to_anticyclone(responsive)
+
+
+# 當前的風力向量，會影響雲的移動和樹的搖擺
+var _wind_speed: float = 0
+var wind_decay_factor = 50.0
+
+func _physics_process(delta: float) -> void:
+	if not Engine.is_editor_hint():
+		_wind_speed = _wind_speed * (wind_decay_factor ** -delta)
+		$CloudManager.update_wind_speed(_wind_speed)
+
+@export var sun_progress_speed: float = 1/30.0
+var sun_should_go: bool = false
+var sun_should_emit_light: bool = false
+
+func _process(_delta: float) -> void:
+
+	if sun_should_go:
+		sunlight_progress += _delta * sun_progress_speed
+	
+	if sun_should_emit_light:
+		emit_light_from_sun()
+
+
+#region 公開 API - 供外部呼叫
+
+# --- 太陽與天空控制 ---
+@export_group("Sky Colors")
+@export var sun_color_over_time : Gradient
+@export var sky_color_over_time : Gradient
+## 設定太陽月亮的進度
+## @param progress: float - 從 0 到 1 的進度, 0 是日出前, 1 是日落後, 0.5 是正中午
+
+
+# 太陽節點，可能是 Sprite2D 或 Light2D
+@export_range(0.0, 1.0) var sunlight_progress: float = 0.0:
+	set(value):
+		# 調整太陽位置
+		sunlight_progress = value
+		if not is_inside_tree(): return
+		$sun_orbit_center.rotation_degrees = lerp(30, -30, value)
+
+		# 調整天空顏色
+		var sky_light_progress = 1.0 - 2.0 * abs(sunlight_progress - 0.5)
+		$sky_background.color = sky_color_over_time.sample(sky_light_progress)
+		# 微調太陽顏色
+		#$sun_orbit_center/sun/Sunball2d.tint_color = sun_color_over_time.sample(sky_light_progress)
+		#$sun_orbit_center/sun/EnvLight2D.color = sun_color_over_time.sample(sky_light_progress)
+
+
+var LightParticleScene = preload("res://levels/solargame/light_particle/light_particle.tscn")
+
+# 發射單個粒子
+func emit_light_from_sun():
+	var particle = LightParticleScene.instantiate()
+	particle.global_position = $sun_orbit_center/sun.global_position
+	
+	var angle = randf_range(0, 2*PI)
+	var speed = 1200#randf_range(200, 400)
+	var initial_velocity = Vector2(cos(angle), sin(angle)) * speed
+	
+	get_parent().add_child(particle)
+	particle.linear_velocity = initial_velocity
+
+
+
+
+## 啟用或禁用環境中的所有碰撞
+func set_collision_enabled(enabled: bool) -> void:
+	$CloudManager.set_collision_enabled(enabled)
+	$SolarPanel2d.set_collision_enabled(enabled)
+
+
+func reset():
+	# Reset wind
+	_wind_speed = 0.0
+
+	# Reset sun and orbit position
+	sun_should_go = false
+	sun_should_emit_light = false
+	self.sunlight_progress = 0.0
+
+	# Reset the CloudManager
+	$CloudManager.reset()
+
+	# 計分板
+	score_board.reset()
+	score_board.modulate.a = 0
+	score_board.position = Vector2(0, 0)
+
+
+func spawn_clouds():
+	$CloudManager.start()
+	get_tree().create_timer(3).timeout.connect($CloudManager.stop_generate)
+
+
+
+func start_game_play():
+	# 太陽開始移動
+	sun_should_go = true
+	sun_should_emit_light = true
+
+	# 雲開生成
+	$CloudManager.start()
+
+
+func stop_game_play():
+	# 太陽停止
+	sun_should_go = false
+	sun_should_emit_light = false
+
+	# 雲集體原地消失
+	$CloudManager.stop_generate()
+	$CloudManager.clear_all_clouds()
+
+
+############ (Almost) Copied from wheelgame_env
+
+
+
+@onready var entire_env = $"."
+var camera_tween
+
+func camera_to(target_center, target_scale=1.0, duration=1, callback=null):
+	if camera_tween:
+		camera_tween.kill()
+
+	var screen_center = size / 2.0
+	var new_position = screen_center - target_center * target_scale
+	camera_tween = create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+	camera_tween.tween_property(entire_env, 'scale', Vector2(target_scale, target_scale), duration)
+	camera_tween.parallel().tween_property(entire_env, 'position', new_position, duration)
+
+	if callback:
+		camera_tween.finished.connect(callback)
+
+
+
+
+@onready var score_board = $ScoreBoard
+var score:
+	get:
+		return score_board.score
+
+func show_score_board(): return score_board.show_score_board()
+
+
+
+
+func collect_electrons():
+	score_board.score = electron_collected
+	all_electron_collected.emit()
